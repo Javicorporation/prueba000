bec34c0b33f6241e905edeeb1cc4fd43059a522b
Javier Carchipulla S. <krchipullajavier@gmail.com>
2025-04-01 22:04:00 -0500

    Implementación de un hambler que evita el programa se muestre en un determinado tiempo, y cuando eso, pase redirigirnos a la otra actividad que es MainActivity Iniciamos la actividad y la cerramos

--- app/src/main/java/com/eje/prueba000/SplashScreem.java
+++ app/src/main/java/com/eje/prueba000/SplashScreem.java
@@ -1,6 +1,8 @@
 package com.eje.prueba000;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SplashScreem extends AppCompatActivity {
 
+    // transcribe
     @Override
+    // Bundle savedInstanceState sirve para almacenar el estaddo anterio de la actividad en caso de que se reinicie
     protected void onCreate(Bundle savedInstanceState) {
+        // el super sirve para llamar a metodo on create y que se genere correctamente
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_screem);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // mostramos la pantalla de carga durante 3 segundos
+        // handler permite
+        new Handler().postDelayed( () -> {
+
+            // Se crea un objeto Intent que indica que queremos ir desde SplashScreen hacia MainActivity.
+            Intent intent = new Intent(SplashScreem.this, MainActivity.class);
+            startActivity(intent);
+            // cierra la actividad actual
+            finish();
+
+        }, 3000);
     }
 }
